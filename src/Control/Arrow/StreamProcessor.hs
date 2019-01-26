8cc56a0de66dd8256600b8a62f6245d74c6af59b
Andreas Westerwick <westerwick@pconas.de>
2019-01-26 21:27:04 +0100

    type class

--- src/Control/Arrow/StreamProcessor.hs
+++ src/Control/Arrow/StreamProcessor.hs
@@ -1,10 +1,36 @@
 {-# LANGUAGE Arrows #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Control.Arrow.StreamProcessor where
 
 import Control.Arrow
-import Control.Applicative
 import Data.Foldable
+import Data.Maybe
+import Data.Vector (Vector, fromListN)
+import Data.Map (Map, toAscList, fromAscList)
+import Prelude hiding (id, (.))
+
+
+class ArrowSelect f a where
+  select :: a b c -> a (f b) (f c)
+
+
+instance ArrowChoice a => ArrowSelect [] a where
+  select = streamProcessor
+
+instance ArrowChoice a => ArrowSelect Maybe a where
+  select f = maybeToList ^>> streamProcessor f >>^ listToMaybe
+
+instance ArrowChoice a => ArrowSelect (Either k) a where
+  select f = right f
+
+instance ArrowChoice a => ArrowSelect Vector a where
+  select f = arr length &&& arr toList >>> second (streamProcessor f) >>^ uncurry fromListN
+
+instance (ArrowChoice a, Eq k) => ArrowSelect (Map k) a where
+  select f = toAscList ^>> unzip ^>> second (streamProcessor f) >>^ uncurry zip >>^ fromAscList
 
 
 streamProcessor :: ArrowChoice a => a b c -> a [b] [c]
