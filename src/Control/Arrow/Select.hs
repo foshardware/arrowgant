3d8fa38fc38033dd456ad16b3c6ca11774c0f9ab
Andreas Westerwick <westerwick@pconas.de>
2019-01-27 11:24:12 +0100

    select instance for tuple

--- src/Control/Arrow/Select.hs
+++ src/Control/Arrow/Select.hs
@@ -15,19 +15,22 @@ class ArrowSelect f a where
   select :: a b c -> a (f b) (f c)
 
 
+instance Arrow a => ArrowSelect ((,) b) a where
+  select = second
+
 instance ArrowChoice a => ArrowSelect [] a where
   select = streamProcessor
 
 instance ArrowChoice a => ArrowSelect Maybe a where
   select f = maybe (Left Nothing) Right ^>> right f >>^ either id Just
 
-instance ArrowChoice a => ArrowSelect (Either k) a where
+instance ArrowChoice a => ArrowSelect (Either b) a where
   select = right
 
 instance ArrowChoice a => ArrowSelect Vector a where
   select f = arr length &&& arr toList >>> second (select f) >>^ uncurry fromListN
 
-instance (ArrowChoice a, Eq k) => ArrowSelect (Map k) a where
+instance (ArrowChoice a, Eq b) => ArrowSelect (Map b) a where
   select f = toAscList ^>> unzip ^>> second (select f) >>^ uncurry zip >>^ fromAscList
 
 
