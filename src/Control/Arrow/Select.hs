90d8866137e098efd8c1e494ca98876c6e60b10f
Andreas Westerwick <westerwick@pconas.de>
2019-02-07 14:01:25 +0100

    speculative execution is branching mechanism

--- src/Control/Arrow/Select.hs
+++ src/Control/Arrow/Select.hs
@@ -2,6 +2,20 @@
 module Control.Arrow.Select where
 
 import Control.Arrow
+import Control.Category
+import Prelude hiding (id)
+
 
 class Arrow a => ArrowSelect a where
+
   select :: Traversable f => a b c -> a (f b) (f c)
+
+
+infixr 4 ///, \\\
+
+class ArrowChoice a => ArrowRace a where
+
+  (///) :: a b c -> a b d -> a b (Either c d)
+
+  (\\\) :: a b c -> a b c -> a b c
+  a \\\ b = a /// b >>> id ||| id
