8ffe39268fad48da014e1251d98d168330fadff7
Andreas Westerwick <westerwick@pconas.de>
2019-05-13 23:31:19 +0200

    arrowselect instance

--- src/Control/Arrow/Select.hs
+++ src/Control/Arrow/Select.hs
@@ -11,6 +11,11 @@ class Arrow a => ArrowSelect a where
   select :: Traversable f => a b c -> a (f b) (f c)
 
 
+instance ArrowSelect (->) where
+
+  select = fmap
+
+
 infixr 4 ///, \\\
 
 class ArrowChoice a => ArrowRace a where
